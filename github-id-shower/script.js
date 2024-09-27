ab64992f325ab7fad41a7f6ed54d370d64e1b284
yash <yashgovind123@gmail.com>
2024-09-28 04:53:18 +0530

    some minor changes

--- github-id-shower/script.js
+++ github-id-shower/script.js
@@ -2,6 +2,7 @@ const searchBtn = document.querySelector(".searchBtn");
 const TextInput = document.querySelector(".UsernameText");
 const goBackBtn = document.querySelector(".go-back");
 const userInfo = document.querySelector(".userInfo");
+const header = document.querySelector(".Heading");
 // event listener for when user clicks the search button.
 searchBtn.addEventListener("click", () => {
   const userName = TextInput.value; //search text
@@ -22,6 +23,7 @@ searchBtn.addEventListener("click", () => {
 
       searchBtn.style.display = "none";
       TextInput.style.display = "none";
+     header.style.display = "none";
       userInfo.innerHTML = `
          <h2> Name : ${data.name}</h2>
          <img src ="${data.avatar_url}"></img>
@@ -29,14 +31,17 @@ searchBtn.addEventListener("click", () => {
          <p>Profile URL: <a href="${data.html_url}">${data.html_url}</a></p>
          <p> Location : ${data.location}</p>
          <p> Public Repositories :${data.public_repos} </p>
-         <p> 
+         <p> Bio : ${data.bio} </p>
+         <p> Following : ${data.following} </p>
+         <h3> FROM : ${data.created_at} </h3>
+         <h3> Last Update : ${data.updated_at}</h3>
          `;
       goBackBtn.style.display = "inline";
     })
     .catch((error) => console.log("error occured ", error)); //if data not present , gimme error'
 });
 goBackBtn.addEventListener("click", () => {
-  userInfo.innerHTML = ""; //clear user info , show the searchbar
+  userInfo.innerHTML = ""; //  show the searchbar
   searchBtn.style.display = "inline";
   TextInput.style.display = "inline";
   goBackBtn.style.display = "";
